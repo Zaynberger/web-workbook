70d7f45dce39baec18d55727c42c143347e85d60
Zayn Berger <zayn.berger@gmail.com>
2018-03-06 20:43:38 -0500

    dompractice

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,5 +1,19 @@
 'use strict';
 
-document.addEventListener("DOMContentLoaded", function(event) {
-    // You code here
-});
+window.onload=function() {
+  var listCountMessage = setListMessage();
+  alert(listCountMessage);
+  //createListCountHeaderElement();
+  //updateListCountHeaderMessage();
+  //createNewItemTextInput();
+  //addRemoveButtonsToExistingItems();
+}
+  function setListMessage() {
+    let list = document.getElementsByTagName("ul");
+    return "This page has "+ list[0].children.length + " Items in the shopping cart";
+  }
+  function createListCountHeaderElement(message){
+    let listHeading= document.createElement("h2");
+    document.getElementsByTagName("h1")[0].appendChild(listHeading);
+    listHeading.setAttribute("id", "list-heading");
+  }
